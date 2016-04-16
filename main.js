cff269061f944c42e671c48f49fbb3aa653372db
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2016-04-16 23:05:21 +0200

    i think i'm there

---
+++ main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const tmx = require('tmx-parser')
+
+const exportToCollisionArray = require('./lib/exportToCollisionArray')
+const exportToNamArray = require('./lib/exportToNamArray')
+const rightMapSize = require('./lib/rightMapSize')
+
+const main = function (tmxfile, collisionFilename, namFilename) {
+  tmx.parseFile(tmxfile, function (err, tmxmap) {
+    if (err) {
+      throw err
+    }
+
+    let firstLayer = tmxmap.layers[0]
+    if (rightMapSize(firstLayer)) {
+      collisionFilename = collisionFilename || tmxfile.split('.tmx')[0] + '_collision.h'
+      namFilename = namFilename || tmxfile.split('.tmx')[0] + '_nametable.h'
+      exportToCollisionArray(firstLayer, collisionFilename)
+      exportToNamArray(firstLayer, namFilename)
+    } else {
+      throw new Error('Map is not the right size for the nes')
+    }
+  })
+}
+
+module.exports = main
---
+++ tests/test.js
@@ -0,0 +1,19 @@
+const nestmx = require('../main')
+nestmx('./tests/data/testmap.tmx')
+const rightMapSize = require('../lib/rightMapSize')
+
+const tape = require('tape')
+
+tape('should check the size of the tmxmap', function (t) {
+  t.plan(2)
+
+  const mockRightSize = {
+    tiles: new Array(64 * 60)
+  }
+  t.equal(rightMapSize(mockRightSize), true)
+
+  const mockWrongSize = {
+    tiles: new Array(30 * 17)
+  }
+  t.equal(rightMapSize(mockWrongSize), false)
+})
